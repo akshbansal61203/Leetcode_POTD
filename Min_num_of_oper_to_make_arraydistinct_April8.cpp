4ce13fde7a397cfabeb5b76401686068b7b13be8
Akshar Bansal <123982568+akshbansal61203@users.noreply.github.com>
2025-04-08 21:45:18 +0530

    Create Min_num_of_oper_to_make_arraydistinct_April8.cpp

---
+++ Min_num_of_oper_to_make_arraydistinct_April8.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(st.count(nums[i])){
+                return (i+1+3-1)/3;
+            }
+            st.insert(nums[i]);
+        }
+        return 0;
+    }
+};
