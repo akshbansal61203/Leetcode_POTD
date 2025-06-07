4ed89fe44e1a8956593ba89086f96c7975a1b907
Akshar Bansal <123982568+akshbansal61203@users.noreply.github.com>
2025-06-07 23:58:36 +0530

    Create Lexically_ministring_removestar_June7.cpp

---
+++ Lexically_ministring_removestar_June7.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    typedef pair<char,int> p;
+    struct comp{
+        bool operator()(p&p1,p&p2){
+            if(p1.first==p2.first){
+                return p1.second<p2.second;
+            }
+            return p1.first>p2.first;
+        }
+    };
+    string clearStars(string s) {
+        int n=s.length();
+        priority_queue<p,vector<p>,comp>pq;
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(s[i]!='*')pq.push({s[i],i});
+            else{
+                int idx=pq.top().second;
+                pq.pop();
+                s[idx]='*';
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(s[i]!='*')ans+=s[i];
+        }
+        return ans;
+    }
+};
