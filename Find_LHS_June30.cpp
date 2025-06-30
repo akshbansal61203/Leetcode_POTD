4e5c223c85ebfec8919421b72450878795aed33f
Akshar Bansal <123982568+akshbansal61203@users.noreply.github.com>
2025-06-30 14:52:25 +0530

    Update Find_LHS_June30.cpp

--- Find_LHS_June30.cpp
+++ Find_LHS_June30.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool f=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]-nums[i-1]==1){
-                f=1;
-            }
-        }
-        if(f==0)return 0;
         map<int,int>mp;
         for(auto it:nums)mp[it]++;
         int ans=0;
